266ecb2155d0bec6572d7dc4632215ce5fc8c15f
starwada <starwada@jcom.home.ne.jp>
2015-10-13 21:54:35 +0900

    update appwidgets作成中

---
+++ app/src/main/java/com/example/wada/myapplication/SoraAppWidget.java
@@ -0,0 +1,65 @@
+package com.example.wada.myapplication;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link SoraAppWidgetConfigureActivity SoraAppWidgetConfigureActivity}
+ */
+public class SoraAppWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            SoraAppWidgetConfigureActivity.deleteTitlePref(context, appWidgetIds[i]);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = SoraAppWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sora_app_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Create an Intent to launch ExampleActivity
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Get the layout for the App Widget and attach an on-click listener
+        // to the button
+        //RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
+
